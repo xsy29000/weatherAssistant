a7dcef5a569441d734c58e9fc7841aa712c5d05d
xsy29000 <ied_xsy@gdhqx.com>
2020-03-04 16:14:40 +0800

    v0.0.1

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/robfig/cron"
 	"time"
 	"weatherAssistant/engine"
 	"weatherAssistant/hefeng"
@@ -10,34 +11,34 @@ import (
 
 func main() {
 
-	//c := cron.New()
-	//spec := "0 43 17 * * ?"
-	//c.AddFunc(spec, func() {
-	t1 := time.Now().Year()   //年
-	t2 := time.Now().Month()  //月
-	t3 := time.Now().Day()    //日
-	t4 := time.Now().Hour()   //小时
-	t5 := time.Now().Minute() //分钟
-	fmt.Printf("%d年%d月%d日 %d:%d\n", t1, t2, t3, t4, t5)
+	c := cron.New()
+	spec := "0 43 17 * * ?"
+	c.AddFunc(spec, func() {
+		t1 := time.Now().Year()   //年
+		t2 := time.Now().Month()  //月
+		t3 := time.Now().Day()    //日
+		t4 := time.Now().Hour()   //小时
+		t5 := time.Now().Minute() //分钟
+		fmt.Printf("%d年%d月%d日 %d:%d\n", t1, t2, t3, t4, t5)
 
-	UserList := []string{"337612001@qq.com"}
+		UserList := []string{"337612001@qq.com"}
 
-	var weatherInfo hefeng.HeFWeather
-	e := engine.Info{
-		WorkerCount: 3,
-		Inf:         inform.Mail{},
-		UserList:    UserList,
-		GettingData: &weatherInfo,
-	}
+		var weatherInfo hefeng.HeFWeather
+		e := engine.Info{
+			WorkerCount: 3,
+			Inf:         inform.Mail{},
+			UserList:    UserList,
+			GettingData: &weatherInfo,
+		}
 
-	e.Run(engine.Request{
-		Url: "https://free-api.heweather.net/s6/weather/lifestyle?location=guangzhou&key=26e6e05426344f8da738eb82bd30e574",
+		e.Run(engine.Request{
+			Url: "https://free-api.heweather.net/s6/weather/lifestyle?location=guangzhou&key=26e6e05426344f8da738eb82bd30e574",
+		})
 	})
-	//})
-	//
-	//c.Start()
-	//
-	//select{}
+
+	c.Start()
+
+	select {}
 
 	return
 }
