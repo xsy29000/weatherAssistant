4f5309c4dad975906c64ed0b2ed0c0941beb0cfd
xsy29000 <ied_xsy@gdhqx.com>
2020-03-09 17:33:11 +0800

    v1.0.2

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/go-gomail/gomail v0.0.0-20160411212932-81ebce5c23df
+	github.com/robfig/cron v1.2.0
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	gopkg.in/gcfg.v1 v1.2.3
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df // indirect
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/robfig/cron"
 	"gopkg.in/gcfg.v1"
 	"time"
 	"weatherAssistant/engine"
@@ -12,56 +13,56 @@ import (
 func main() {
 
 	//定時任務
-	//c := cron.New()
-	//spec := "0 43 17 * * ?"
-	//c.AddFunc(spec, func() {
-	t1 := time.Now().Year()   //年
-	t2 := time.Now().Month()  //月
-	t3 := time.Now().Day()    //日
-	t4 := time.Now().Hour()   //小时
-	t5 := time.Now().Minute() //分钟
-	fmt.Printf("%d年%d月%d日 %d:%d\n", t1, t2, t3, t4, t5)
+	c := cron.New()
+	spec := "0 0 8 * * ?"
+	c.AddFunc(spec, func() {
+		t1 := time.Now().Year()   //年
+		t2 := time.Now().Month()  //月
+		t3 := time.Now().Day()    //日
+		t4 := time.Now().Hour()   //小时
+		t5 := time.Now().Minute() //分钟
+		fmt.Printf("%d年%d月%d日 %d:%d\n", t1, t2, t3, t4, t5)
 
-	UserList := []string{"337612001@qq.com"}
+		UserList := []string{"337612001@qq.com"}
 
-	var weatherInfo hefeng.HeFWeather
+		var weatherInfo hefeng.HeFWeather
 
-	//读取邮件发送模块配置
-	var mail inform.MailIniStr
+		//读取邮件发送模块配置
+		var mail inform.MailIniStr
 
-	iniInfo := struct {
-		HeFeng struct {
-			Url string
+		iniInfo := struct {
+			HeFeng struct {
+				Url string
+			}
+			Email struct {
+				Sender         string
+				SenderPassword string
+			}
+		}{}
+		err := gcfg.ReadFileInto(&iniInfo, "config.ini")
+		if err != nil {
+			fmt.Printf("Failed to parse config file: %s\n", err)
+			return
 		}
-		Email struct {
-			Sender         string
-			SenderPassword string
+
+		mail = iniInfo.Email
+		e := engine.Info{
+			WorkerCount: 3,
+			Inf: inform.Mail{
+				MailIni: mail,
+			},
+			UserList:    UserList,
+			GettingData: &weatherInfo,
 		}
-	}{}
-	err := gcfg.ReadFileInto(&iniInfo, "config.ini")
-	if err != nil {
-		fmt.Printf("Failed to parse config file: %s\n", err)
-		return
-	}
 
-	mail = iniInfo.Email
-	e := engine.Info{
-		WorkerCount: 3,
-		Inf: inform.Mail{
-			MailIni: mail,
-		},
-		UserList:    UserList,
-		GettingData: &weatherInfo,
-	}
-
-	e.Run(engine.Request{
-		Url: iniInfo.HeFeng.Url,
+		e.Run(engine.Request{
+			Url: iniInfo.HeFeng.Url,
+		})
 	})
-	//})
-	//
-	//c.Start()
-	//
-	//select {}
+
+	c.Start()
+
+	select {}
 
 	return
 }
